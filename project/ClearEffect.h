c3f9d0cba9451dc91a87692c64323de058467e88
oosakiharuki <k023g0035@g.neec.ac.jp>
2026-07-23 15:35:55 +0900

    クリアエフェクト作成

---
+++ project/ClearEffect.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Particle.h"
+
+/// <summary>
+/// 
+/// </summary>
+class ClearEffect {
+public:
+
+	void Initialize();
+
+private:
+	std::string name = "clear_Effect";
+};
+
